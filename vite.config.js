7204329dfd109cfcfe845d5730ba4b3b4614f4a3
Nacho Codoñer <igcogi@gmail.com>
2024-03-25 18:04:50 +0100

    adapt to the simplest form of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
-import stripCode from 'rollup-plugin-strip-code';
 import { meteor } from 'meteor-vite/plugin';
 
-const isDevelopment = process.env.npm_lifecycle_script?.includes('mode=development');
-const enableBundleVisualizer = process.env.ENABLE_BUNDLE_VISUALIZER === 'true';
-
-const developmentVsProductionDefineConfig = isDevelopment ? {
-    'Meteor.isDevelopment': JSON.stringify(true),
-    'Meteor.isProduction': JSON.stringify(false),
-} : {
-    'Meteor.isDevelopment': JSON.stringify(false),
-    'Meteor.isProduction': JSON.stringify(true),
-};
-
-function excludeBlockStrip(excludeConfig) {
-    return excludeConfig?.exclude
-        ? {
-            ...stripCode({
-                start_comment: `${excludeConfig.exclude}:start`,
-                end_comment: `${excludeConfig.exclude}:end`,
-            }),
-            enforce: 'pre',
-            apply: 'build',
-        }
-        : undefined;
-}
-
-const excludeBlockStripByMode = isDevelopment
-    ? excludeBlockStrip({ exclude: 'production' })
-    : excludeBlockStrip({ exclude: 'development' });
-
-const clientCommonConfig = {
-    build: {
-        outDir: 'client',
-        emptyOutDir: false,
-        target: 'modules',
-        rollupOptions: {
-            external: [
-                /^(meteor.*|react|react-dom)/,
-            ],
-            plugins: [
-                enableBundleVisualizer && visualizer({ open: true, filename: 'public/stats.html' }),
-            ].filter(Boolean),
-        },
-        minify: false,
-    },
-    define: {
-        'Meteor.isClient': JSON.stringify(true),
-        'Meteor.isServer': JSON.stringify(false),
-        'Meteor.isTest': JSON.stringify(false),
-        ...developmentVsProductionDefineConfig,
-    },
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
     plugins: [
         meteor({
             clientEntry: './ui/main.jsx',
@@ -65,31 +16,5 @@ const clientCommonConfig = {
         react({
             jsxRuntime: 'classic',
         }),
-        excludeBlockStrip({ exclude: 'server' }),
-        excludeBlockStripByMode,
     ],
-};
-
-const clientDevelopmentConfig = {
-    ...clientCommonConfig,
-};
-
-const clientProductionConfig = {
-    ...clientCommonConfig,
-    build: {
-        ...clientCommonConfig.build,
-        minify: true,
-    },
-};
-
-// eslint-disable-next-line import/no-default-export
-export default defineConfig(({ mode }) => {
-    switch (mode) {
-        case 'development':
-            return clientDevelopmentConfig;
-        case 'production':
-            return clientProductionConfig;
-        default:
-            return clientProductionConfig;
-    }
 });
